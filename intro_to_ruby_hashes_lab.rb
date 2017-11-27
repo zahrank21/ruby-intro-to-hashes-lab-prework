9ff74d1bfd3eb1101133a01d61186e8bd55f71ae
zahrank21 <zahrank@gmail.com>
2017-11-27 14:13:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,11 @@ monopoly = {railroads: {
 end
 
 def monopoly_with_third_tier
-	
+	monopoly = {railroads: {
+	  pieces: 4,
+	  names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {}},
+	  rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
+	  }}
 end
 
 def monopoly_with_fourth_tier
